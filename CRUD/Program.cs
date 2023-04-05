07f6fd08666d5cef23171f01d1a5a211ea2d944c
Damar Saputra <damarsaputra2626@gmail.com>
2023-04-05 17:06:20 +0700

    first commit

--- CRUD/Program.cs
+++ CRUD/Program.cs
@@ -1,5 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Create Data Base");
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Create Data Base");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Create_Table
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+        }
+
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-VHM1FF8V\\SQLEXPRESS;" + "database=cucisepatu;User ID=sa;Password=Masamay2601");
+
+                con.Open();
+
+                SqlCommand cm = new SqlCommand(" Create Table Mahasiswa_Coba (NIM char(12) not null " +
+                    "primary key, Nama varchar(50), Alamat varchar(225), Jenis_Kelamin char(1))", con);
+
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Tabel Sukses Dibuat");
+                Console.ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OPPs, ada yang salah." + e);
+                Console.ReadKey();
+            }
+
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
 
 
